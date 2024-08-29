22d27cdb82ca78515eb2a282a7a541b394b72fa7
Rustam <rustam.kerimli.wf@gmail.com>
2024-08-30 00:44:59 +0400

    Popup Image and Slider

---
+++ src/components/PopupImage/PopupImage.jsx
@@ -0,0 +1,53 @@
+import {useState, useEffect} from "react";
+import "./popup-image.scss";
+
+const PopupImage = ({src, className, alt}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 992);
+
+  const togglePopup = () => {
+    if (isLargeScreen) {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 992);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    if (isOpen && isLargeScreen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isOpen, isLargeScreen]);
+
+  return (
+    <div>
+      <img
+        src={src}
+        alt={alt}
+        onClick={togglePopup}
+        className={`thumbnail-image ${className}`}
+      />
+
+      {isOpen && isLargeScreen && (
+        <div className="popup-overlay" onClick={togglePopup}>
+          <div className="popup-content">
+            <img src={src} alt={alt} className="popup-image" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PopupImage;
--- src/sections/SinglePropertySection/SinglePropertySection.jsx
+++ src/sections/SinglePropertySection/SinglePropertySection.jsx
@@ -6,26 +6,35 @@ import PinIcon from "../../assets/icons/pin.svg";
 import BedIcon from "../ListingSection/image/bed.svg";
 import BathIcon from "../ListingSection/image/bathtub.svg";
 import MoveIcon from "../ListingSection/image/move.svg";
-import ProductImg from "../../assets/images/11.jpg";
 import {Link} from "react-router-dom";
 import CommentBlock from "../../components/CommentBlock/CommentBlock";
-import CommentImg1 from "../../assets/images/comment-1.jpg";
 import MsgPhoneIcon from "../../assets/icons/msg-telephone-icon.png";
 import Slider from "../../components/Slider/Slider";
 import {useLocation} from "react-router-dom";
+import {useSelector} from "react-redux";
+import PopupImage from "../../components/PopupImage/PopupImage";
 
 const SinglePropertySection = () => {
   const location = useLocation();
-
+  const propertyPosts = useSelector(
+    (state) => state.propertyPosts.propertyPosts
+  );
   const pathname = location.pathname;
   const lastPathItem = pathname.split("/").filter(Boolean).pop();
+  const prop_id = (lastPathItem % 17) + 1;
+
+  const property = propertyPosts.find((item) => {
+    return +item.id === +lastPathItem;
+  });
+
+  const agents = useSelector((state) => state.agents.agents);
 
   const getItems = () => {
-    const src_1 = `/public/properties/${lastPathItem}-1.jpg`;
-    const src_2 = `/public/properties/${lastPathItem}-2.jpg`;
-    const src_3 = `/public/properties/${lastPathItem}-3.jpg`;
-    const src_4 = `/public/properties/${lastPathItem}-4.jpg`;
-    const src_5 = `/public/properties/${lastPathItem}-5.jpg`;
+    const src_1 = `/public/properties/${prop_id}-1.jpg`;
+    const src_2 = `/public/properties/${prop_id}-2.jpg`;
+    const src_3 = `/public/properties/${prop_id}-3.jpg`;
+    const src_4 = `/public/properties/${prop_id}-4.jpg`;
+    const src_5 = `/public/properties/${prop_id}-5.jpg`;
     return [
       {id: 1, src: src_1},
       {id: 2, src: src_2},
@@ -35,6 +44,10 @@ const SinglePropertySection = () => {
     ];
   };
 
+  const agent = agents.find((item) => {
+    return +item.id === +property.agents_id;
+  });
+
   return (
     <>
       <Slider items={getItems()} />
@@ -42,15 +55,25 @@ const SinglePropertySection = () => {
         <div className="property-left-block">
           <div className="property-block">
             <div className="property-name-status-block">
-              <span className="property-status">Rent</span>
-              <h3 className="property-name">4113 Holiday Drive</h3>
+              <span
+                className={`property-status ${
+                  property?.type_id === 2 ? "red" : ""
+                }`}
+              >{`${property?.type_id === 1 ? "Rent" : "Sale"}`}</span>
+              <h3 className="property-name">{property?.title}</h3>
               <span className="property-address">
                 <Wrapper width={"1rem"}>
                   <Image src={PinIcon} className={"property-name-icon"} />
                 </Wrapper>
-                5799 Boehm Grove Brekkehaven, SD 61992, Hampton
+                {property?.address}
               </span>
-              <h3 className="property-price">$175,939 / monthly</h3>
+              <h3 className="property-price">
+                {" "}
+                {`$${property?.price
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              ${property?.type_id === 1 ? " / monthly" : ""}`}
+              </h3>
               <div className="list-fx-features">
                 <div className="listing-card-info-icon">
                   <div className="inc-fleat-icon">
@@ -58,7 +81,7 @@ const SinglePropertySection = () => {
                       <Image src={BedIcon} />
                     </Wrapper>
                   </div>
-                  3 Beds
+                  {property?.beds} Beds
                 </div>
                 <div className="listing-card-info-icon">
                   <div className="inc-fleat-icon">
@@ -66,7 +89,7 @@ const SinglePropertySection = () => {
                       <Image src={BathIcon} />
                     </Wrapper>
                   </div>
-                  1 Bath
+                  {property?.baths} Bath
                 </div>
                 <div className="listing-card-info-icon">
                   <div className="inc-fleat-icon">
@@ -74,7 +97,7 @@ const SinglePropertySection = () => {
                       <Image src={MoveIcon} />
                     </Wrapper>
                   </div>
-                  290 m²
+                  {property?.area} m²
                 </div>
               </div>
             </div>
@@ -84,16 +107,13 @@ const SinglePropertySection = () => {
             <div className="block-body">
               <ul className="detail-features">
                 <li className="detail-features-li">
-                  <strong>Bedrooms:</strong> 1 Beds
+                  <strong>Bedrooms:</strong> {property?.beds} Beds
                 </li>
                 <li className="detail-features-li">
-                  <strong>Bathrooms:</strong> 2 Bath
+                  <strong>Bathrooms:</strong> {property?.baths} Bath
                 </li>
                 <li className="detail-features-li">
-                  <strong>Square:</strong>112 m²
-                </li>
-                <li className="detail-features-li">
-                  <strong>Floors:</strong>3
+                  <strong>Square:</strong> {property?.area} m²
                 </li>
                 <li className="detail-features-li">
                   <strong>Property Type:</strong>House
@@ -103,92 +123,23 @@ const SinglePropertySection = () => {
           </div>
           <div className="property-block">
             <h4 className="property-block-title">Description</h4>
-            <p>
-              Soup of the fact. 'I keep them to sell,' the Hatter asked
-              triumphantly. Alice did not sneeze, were the cook, and a large
-              ring, with the game,' the Queen to play croquet.' Then they both
-              cried. 'Wake up, Alice dear!' said her sister; 'Why, what are they
-              made of?' 'Pepper, mostly,' said the Eaglet. 'I don't think they
-              play at all a pity. I said "What for?"' 'She boxed the Queen's
-              hedgehog just now, only it ran away when it grunted again, so
-              violently, that she was shrinking rapidly; so she began shrinking
-              directly. As soon as there was no use going back to finish his
-              story. CHAPTER IV. The Rabbit started violently, dropped the white
-              kid gloves: she took up the other, saying, in a large caterpillar,
-              that was said, and went on without attending to her; 'but those
-              serpents! There's no pleasing them!' Alice was very glad she had
-              expected: before she made it out again, so violently, that she had
-              felt quite unhappy at the great question certainly was, what?
-              Alice looked at her, and the. With gently smiling jaws!' 'I'm sure
-              those are not the right size to do anything but sit with its
-              eyelids, so he with his head!' she said, without even looking
-              round. 'I'll fetch the executioner went off like an honest man.'
-              There was certainly not becoming. 'And that's the queerest thing
-              about it.' The jury all brightened up at the Gryphon hastily. 'Go
-              on with the time,' she said, 'and see whether it's marked "poison"
-              or not'; for she had not gone We know it to make ONE respectable
-              person!'.The first thing she heard something like this:-- 'Fury
-              said to the other players, and shouting 'Off with their fur
-              clinging close to her: its face to see what was on the top with
-              its mouth and yawned once or twice, and shook itself. Then it got
-              down off the top of it. She felt that it might appear to others
-              that what you were never even spoke to Time!' 'Perhaps not,' Alice
-              replied in an angry tone, 'Why, Mary Ann, and be turned out of the
-              court with a soldier on each side to guard him; and near the
-              centre of the creature, but on second thoughts she decided to
-              remain where she was about a whiting to a mouse, That he met in
-              the world am I? Ah, THAT'S the great question certainly was, what?
-              Alice looked up, but it is.' 'I quite agree with you,' said the
-              Dormouse; 'VERY ill.' Alice tried to curtsey as she could not
-              swim. He sent them word I had not gone (We know it to make out
-              that it was the only difficulty was, that you weren't to talk
-              nonsense. The Queen's argument was, that if you. Who Stole the
-              Tarts? The King laid his hand upon her knee, and looking anxiously
-              about as curious as it went. So she set to work shaking him and
-              punching him in the sky. Alice went on, 'What's your name, child?'
-              'My name is Alice, so please your Majesty?' he asked. 'Begin at
-              the stick, and held out its arms folded, quietly smoking a long
-              hookah, and taking not the smallest notice of them attempted to
-              explain the mistake it had grown to her great disappointment it
-              was empty: she did not dare to laugh; and, as she did so, very
-              carefully, remarking, 'I really must be kind to them,' thought
-              Alice, 'shall I NEVER get any older than I am now? That'll be a
-              Caucus-race.' 'What IS the fun?' said Alice. The poor little juror
-              (it was Bill, the Lizard) could not think of any one; so, when the
-              tide rises and sharks are around, His voice has a timid and
-              tremulous sound.) 'That's different from what I say--that's the
-              same thing with you,' said the Gryphon. 'How the creatures
-              wouldn't be so easily.
-            </p>
+            <p>{property?.description}</p>
           </div>
           <div className="property-block">
             <h4 className="property-block-title">Gallery</h4>
             <ul className="list-gallery-inline">
-              <li className="gallery-li">
-                <Link className="mfp-gallery">
-                  <Image src={ProductImg} className={"mfp-gallery-image"} />
-                </Link>
-              </li>
-              <li className="gallery-li">
-                <Link className="mfp-gallery">
-                  <Image src={ProductImg} className={"mfp-gallery-image"} />
-                </Link>
-              </li>
-              <li className="gallery-li">
-                <Link className="mfp-gallery">
-                  <Image src={ProductImg} className={"mfp-gallery-image"} />
-                </Link>
-              </li>
-              <li className="gallery-li">
-                <Link className="mfp-gallery">
-                  <Image src={ProductImg} className={"mfp-gallery-image"} />
-                </Link>
-              </li>
-              <li className="gallery-li">
-                <Link className="mfp-gallery">
-                  <Image src={ProductImg} className={"mfp-gallery-image"} />
-                </Link>
-              </li>
+              {getItems().map((item) => {
+                return (
+                  <li key={item.id} className="gallery-li">
+                    <div className="mfp-gallery">
+                      <PopupImage
+                        src={item?.src}
+                        className={"mfp-gallery-image"}
+                      />
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <div className="property-block">
@@ -206,61 +157,61 @@ const SinglePropertySection = () => {
           <div className="msg-block">
             <div className="agent-msg-header">
               <div className="agent-msg-image">
-                <Image className={"agent-image"} src={CommentImg1} />
+                <Image className={"agent-image"} src={agent?.photo} />
               </div>
               <div className="msg-block-info">
                 <h4>
-                  <Link className="msg-agent-name" to={"/agent-details/:id"}>
-                    {" "}
-                    America Gottlieb
+                  <Link
+                    className="msg-agent-name"
+                    to={`/agent-details/${agent?.id}`}
+                  >
+                    {agent?.name}
                   </Link>
                 </h4>
                 <div className="msg-agent-phone">
                   <Wrapper width={"1rem"}>
                     <Image src={MsgPhoneIcon} />
                   </Wrapper>
-                  <p className="msg-agent-number">+18386611830</p>
+                  <p className="msg-agent-number">{agent?.phone}</p>
                 </div>
               </div>
             </div>
-            <div className="msg-form-wrapper">
-              <form>
-                <input
-                  className="msg-form-input"
-                  type="text"
-                  placeholder="Name *"
-                  required
-                />
-                <input
-                  className="msg-form-input"
-                  type="text"
-                  placeholder="Phone *"
-                  required
-                />
-                <input
-                  className="msg-form-input"
-                  type="email"
-                  placeholder="Email"
-                  required
-                />
-                <input
-                  type="text"
-                  className="msg-form-input not-allow"
-                  placeholder="4113 Holiday Drive"
-                  readOnly
-                />
-                <textarea
-                  className="msg-form-input text-area"
-                  type="email"
-                  placeholder="Message"
-                  row="5"
-                  required
-                ></textarea>
-                <button className="msg-form-btn" type="submit">
-                  Send Message
-                </button>
-              </form>
-            </div>
+            <form className="msg-form">
+              <input
+                className="msg-form-input"
+                type="text"
+                placeholder="Name *"
+                required
+              />
+              <input
+                className="msg-form-input"
+                type="text"
+                placeholder="Phone *"
+                required
+              />
+              <input
+                className="msg-form-input"
+                type="email"
+                placeholder="Email"
+                required
+              />
+              <input
+                type="text"
+                className="msg-form-input not-allow"
+                placeholder={`${property?.title}`}
+                readOnly
+              />
+              <textarea
+                className="msg-form-input text-area"
+                type="email"
+                placeholder="Message"
+                row="5"
+                required
+              ></textarea>
+              <button className="msg-form-btn" type="submit">
+                Send Comment
+              </button>
+            </form>
           </div>
         </div>
       </Section>
